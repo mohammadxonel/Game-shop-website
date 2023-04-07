393209505454294707435bdbcdc63f9509130d6b
moahammadxxali <ali_mohammad@t-online.de>
2023-04-07 18:25:07 +0200

    add js app

---
+++ app.js
@@ -0,0 +1,5 @@
+const navItemDropDownList = document.querySelectorAll('.nav-item-dropdown');
+const navTogglerOpen = document.querySelector('.navbar-toggler-open');
+const navTogglerClose = document.querySelector('.navbar-toggler-close');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+
